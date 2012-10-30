8fd5548233df1ff332e9d7a58354495aa7cd8879
Simon Stewart <simon.m.stewart@gmail.com>
2012-10-30 17:54:08 +0100

    Adding tests for page loading using the normal page loading strategy

--- navigation/get_from_http_test.py
+++ navigation/get_from_http_test.py
@@ -25,7 +25,30 @@ class GetFromHttpTest(base_test.WebDriverBaseTest):
         expected = self.webserver.where_is('navigation/empty.html')
         url = self.driver.current_url
         self.assertEquals(expected, url)
+        
+    def testGetWillFollowMetaRefreshThatRefreshesInstantly(self):
+        page = self.webserver.where_is('navigation/instant-meta-redirect.html')
+        self.driver.get(page)
+        
+        expected = self.webserver.where_is('navigation/empty.html')
+        url = self.driver.current_url
+        self.assertEquals(expected, url)
 		
-		
+    def testGetWillFollowMetaRefreshThatRefreshesAfterOneSecond(self):
+        page = self.webserver.where_is('navigation/1s-meta-redirect.html')
+        self.driver.get(page)
+
+        expected = self.webserver.where_is('navigation/empty.html')
+        url = self.driver.current_url
+        self.assertEquals(expected, url)		
+
+    def testGetWillNotFollowMetaRefreshThatRefreshesAfterMoreThanOneSecond(self):
+        page = self.webserver.where_is('navigation/60s-meta-redirect.html')
+        self.driver.get(page)
+
+        url = self.driver.current_url
+        self.assertEquals(page, url)		
+
+				
 if __name__ == '__main__':
     unittest.main()
