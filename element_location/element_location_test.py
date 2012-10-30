910c586abde3aa97cca9f1724c466cd7b88bd26d
Ken Kania <kkania@google.com>
2012-10-30 10:06:31 -0700

    Add new element finding tests for single element lookup

--- element_location/element_location_test.py
+++ element_location/element_location_test.py
@@ -4,10 +4,42 @@ import base_test
 
 class ElementLocationTest(base_test.WebDriverBaseTest):
 
-    def testShouldFindElementById(self):
-        self.driver.get(self.webserver.where_is("element_location/elements.html"))
-        e = self.driver.find_element_by_id("myid")
-        self.assertEquals("myidtext", e.text)
+    ELEMENTS_PATH = "element_location/elements.html"
+
+    def testFindElementById(self):
+        self.driver.get(self.webserver.where_is(self.ELEMENTS_PATH))
+        e = self.driver.find_element_by_id("id")
+        self.assertEquals("id", e.text)
+
+
+    def testFindElementByName(self):
+        self.driver.get(self.webserver.where_is(self.ELEMENTS_PATH))
+        e = self.driver.find_element_by_name("name")
+        self.assertEquals("name", e.text)
+
+
+    def testFindElementByCssSelector(self):
+        self.driver.get(self.webserver.where_is(self.ELEMENTS_PATH))
+        e = self.driver.find_element_by_css_selector("#id")
+        self.assertEquals("id", e.text)
+
+
+    def testFindElementByLinkText(self):
+        self.driver.get(self.webserver.where_is(self.ELEMENTS_PATH))
+        e = self.driver.find_element_by_link_text("link text")
+        self.assertEquals("link text", e.text)
+
+
+    def testFindElementByPartialLinkText(self):
+        self.driver.get(self.webserver.where_is(self.ELEMENTS_PATH))
+        e = self.driver.find_element_by_partial_link_text("link tex")
+        self.assertEquals("link text", e.text)
+
+
+    def testFindElementByXPath(self):
+        self.driver.get(self.webserver.where_is(self.ELEMENTS_PATH))
+        e = self.driver.find_element_by_xpath("//*[@id='id']")
+        self.assertEquals("id", e.text)
 
 
 if __name__ == '__main__':
