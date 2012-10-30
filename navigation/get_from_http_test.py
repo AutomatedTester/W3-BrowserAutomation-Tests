1cb861ea9b73ea519fd4bf6c4673868edb8b3470
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-10-30 18:02:20 +0100

    Test that browser does not reload page when navigation to a fragment on current page

--- navigation/get_from_http_test.py
+++ navigation/get_from_http_test.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 import os
- 
+
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 import base_test
@@ -16,33 +16,33 @@ class GetFromHttpTest(base_test.WebDriverBaseTest):
 
         url = self.driver.current_url
         self.assertEquals(page, url)
-        
+
 
     def testGetWillFollowTheLocationHeader(self):
         page = self.webserver.where_is('navigation/redirect')
         self.driver.get(page)
-        
+
         expected = self.webserver.where_is('navigation/empty.html')
         url = self.driver.current_url
         self.assertEquals(expected, url)
-        
-        
+
+
     def testGetWillFollowMetaRefreshThatRefreshesInstantly(self):
         page = self.webserver.where_is('navigation/instant-meta-redirect.html')
         self.driver.get(page)
-        
+
         expected = self.webserver.where_is('navigation/empty.html')
         url = self.driver.current_url
         self.assertEquals(expected, url)
-		
-		
+
+
     def testGetWillFollowMetaRefreshThatRefreshesAfterOneSecond(self):
         page = self.webserver.where_is('navigation/1s-meta-redirect.html')
         self.driver.get(page)
 
         expected = self.webserver.where_is('navigation/empty.html')
         url = self.driver.current_url
-        self.assertEquals(expected, url)		
+        self.assertEquals(expected, url)
 
 
     def testGetWillNotFollowMetaRefreshThatRefreshesAfterMoreThanOneSecond(self):
@@ -50,8 +50,19 @@ class GetFromHttpTest(base_test.WebDriverBaseTest):
         self.driver.get(page)
 
         url = self.driver.current_url
-        self.assertEquals(page, url)		
+        self.assertEquals(page, url)
 
-				
+
+    def testGetFragmentInCurrentDocumentDoesNotReloadPage(self):
+        page = self.webserver.where_is("navigation/fragment.html")
+        fragment_page = "%s#%s" % (page, "fragment")
+
+        self.driver.get(page)
+        self.driver.execute_script("state = true")
+
+        self.driver.get(fragment_page)
+        self.assertEquals(True, self.driver.execute_script("return state"))
+
+
 if __name__ == '__main__':
     unittest.main()
